3a56fe88a7bd4fea253b043cf58fec00f2a4f899
Kayki Letieri <kaykiletieri37@gmail.com>
2024-01-15 08:12:00 -0300

    feat: add category controller

---
+++ ServicesExample/ProductAPI/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Application.DTOs;
+using ProductAPI.Application.Services.Interfaces;
+
+namespace ProductAPI.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class CategoryController(ICategoryService categoryService) : ControllerBase
+{
+
+    private readonly ICategoryService _categoryService = categoryService;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CategoryReadDTO>?>> GetCategoriesAsync()
+    {
+        try
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            if (categories is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(categories);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+}
--- ServicesExample/ProductAPI/Program.cs
+++ ServicesExample/ProductAPI/Program.cs
@@ -10,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
