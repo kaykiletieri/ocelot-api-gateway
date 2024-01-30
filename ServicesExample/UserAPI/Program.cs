0b763b8e717ffba5811efeea5f8fca7ed9a7f9ee
Kayki Letieri <kaykiletieri37@gmail.com>
2024-01-30 14:44:35 -0300

    feat: clear user api

--- ServicesExample/UserAPI/Program.cs
+++ ServicesExample/UserAPI/Program.cs
@@ -10,7 +10,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseDeveloperExceptionPage();        
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
